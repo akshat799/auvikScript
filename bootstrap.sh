5d65c53117dfa07d04d6256c3262974cd17c760d
akshat799 <gulati.akshat799@gmail.com>
2025-06-08 13:54:50 -0300

    updated REPO_URL

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 set -e
 
-REPO_URL="https://github.com/akshat799/auvikScript/main"
+REPO_URL="https://raw.githubusercontent.com/akshat799/auvikScript/main"
 
 curl -fsSL "$REPO_URL/install.sh" -o install.sh
 curl -fsSL "$REPO_URL/.env.gpg" -o .env.gpg
